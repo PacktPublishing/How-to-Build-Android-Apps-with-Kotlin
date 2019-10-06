a4ed5567f0ab34cbe20295a754f5b8cd8eb3a0c9
alexforrester <aforresterwork@gmail.com>
2019-10-06 20:27:44 +0100

    Adds Chapter 2 Save and Restore Exercise

---
+++ Chapter2/SaveAndRestore/app/src/main/java/com/example/saveandrestore/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.saveandrestore
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+
+        discount_button.setOnClickListener {
+
+            val generatedDiscountCode = UUID.randomUUID().toString().take(8).toUpperCase()
+            hideKeyboard()
+            discount_code.text = generatedDiscountCode
+            discount_code_confirmation.text = getString(R.string.discount_code_confirmation)
+
+        }
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    private fun hideKeyboard() {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+    }
+}
