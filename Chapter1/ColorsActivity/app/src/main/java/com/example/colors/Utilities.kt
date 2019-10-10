d5ba367eb6854cfa27ac6a745e114bd213bac9f9
alexforrester <aforresterwork@gmail.com>
2019-10-10 20:26:14 +0100

    Updates range of hexadecimal characters with lower case a-f range

--- Chapter1/ColorsActivity/app/src/main/java/com/example/colors/Utilities.kt
+++ Chapter1/ColorsActivity/app/src/main/java/com/example/colors/Utilities.kt
@@ -1,7 +1,9 @@
 package com.example.colors
 
 
-val hexadecimalCharacters = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B","C", "D", "E", "F" )
+val hexadecimalCharacters = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B","C", "D", "E", "F", "a", "b", "c", "d", "e", "f")
+
 const val HEXADECIMAL_STRING_LENGTH = 6
 
 /**
