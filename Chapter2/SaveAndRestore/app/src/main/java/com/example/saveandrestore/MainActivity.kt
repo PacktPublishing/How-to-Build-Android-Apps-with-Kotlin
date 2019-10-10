4d23d782a7b06019cfa3c24918c8813b59b2043e
alexforrester <aforresterwork@gmail.com>
2019-10-10 20:28:21 +0100

    Removes extraneous import statement

--- Chapter2/SaveAndRestore/app/src/main/java/com/example/saveandrestore/MainActivity.kt
+++ Chapter2/SaveAndRestore/app/src/main/java/com/example/saveandrestore/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
