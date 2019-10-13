a34933a82c93b59b63390b9e984f5d4224f9358b
alexforrester <aforresterwork@gmail.com>
2019-10-13 09:36:38 +0100

    Adds long form technique for using a ViewClickListener with full Single Access Method syntax

--- Chapter1/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Chapter1/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -21,34 +24,33 @@ class MainActivity : AppCompatActivity() {
         //Get the last name TextInputEditText value
         val lastName = (last_name as EditText).text
 
-        //Set a click listener on the button so we can respond to the button click
-        enter_button.setOnClickListener {
-            //Set the text to an empty string when the button is pressed initially
+        enter_button.setOnClickListener( object : View.OnClickListener{
+            override fun onClick(v: View?) {
 
-            //Check the names are not blank trimming any leading or trailing spaces
-            if (firstName.trim().isNotBlank() && lastName.trim().isNotBlank()) {
+                //Check the names are not blank trimming any leading or trailing spaces
+                if (firstName.trim().isNotBlank() && lastName.trim().isNotBlank()) {
 
-                //Create the greeting followed by the full name
-                val nameWithGreeting = StringBuilder()
-                nameWithGreeting.append(getString(R.string.welcome_to_the_app))
-                nameWithGreeting.append(" ")
-                nameWithGreeting.append(firstName)
-                nameWithGreeting.append(" ")
-                nameWithGreeting.append(lastName)
-                nameWithGreeting.append("!")
+                    //Create the greeting followed by the full name
+                    val nameWithGreeting = StringBuilder()
+                    nameWithGreeting.append(getString(R.string.welcome_to_the_app))
+                    nameWithGreeting.append(" ")
+                    nameWithGreeting.append(firstName)
+                    nameWithGreeting.append(" ")
+                    nameWithGreeting.append(lastName)
+                    nameWithGreeting.append("!")
 
-                //Sets the greeting_display text
-                greeting_display.text = nameWithGreeting.toString()
+                    //Sets the greeting_display text
+                    greeting_display.text = nameWithGreeting.toString()
 
-            } else {
-                //Displays a pop-up message informing the user they haven't entered the name correctly
-                val toast =
-                    Toast.makeText(this, getString(R.string.please_enter_a_name), Toast.LENGTH_LONG)
-                toast.setGravity(Gravity.CENTER, 0, 0)
-                toast.show()
+                } else {
+                    //Displays a pop-up message informing the user they haven't entered the name correctly
+                    val toast = Toast.makeText(this@MainActivity, getString(R.string.please_enter_a_name), Toast.LENGTH_LONG)
+                    toast.setGravity(Gravity.CENTER, 0, 0)
+                    toast.show()
 
+                }
             }
-        }
+        })
     }
 }
 
