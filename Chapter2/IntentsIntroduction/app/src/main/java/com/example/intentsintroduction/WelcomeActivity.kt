34b38bd22dffef6961703618b8b2b0398997ca84
Alex Forrester <aforresterwork@gmail.com>
2019-10-18 07:19:11 +0100

    Adds Intents Introduction exercise

---
+++ Chapter2/IntentsIntroduction/app/src/main/java/com/example/intentsintroduction/WelcomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.intentsintroduction
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
