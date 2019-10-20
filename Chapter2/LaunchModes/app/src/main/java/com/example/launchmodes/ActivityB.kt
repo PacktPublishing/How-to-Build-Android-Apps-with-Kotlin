0fa5d667e6933183e8aeb3c44ea9bced028fb016
alexforrester <aforresterwork@gmail.com>
2019-10-20 20:15:43 +0100

    Adds Launch Modes exercise

---
+++ Chapter2/LaunchModes/app/src/main/java/com/example/launchmodes/ActivityB.kt
@@ -0,0 +1,12 @@
+package com.example.launchmodes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityB : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+    }
+}
---
+++ Chapter2/LaunchModes/app/src/main/java/com/example/launchmodes/ActivityOne.kt
@@ -0,0 +1,12 @@
+package com.example.launchmodes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one)
+    }
+}
