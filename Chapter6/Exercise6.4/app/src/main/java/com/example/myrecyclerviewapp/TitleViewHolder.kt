b266f5b9a6eb95124fde5a5a032168ea125732f0
eranboudjnah <shadowcra@gmail.com>
2019-11-19 13:10:50 +0000

    Exercise 6.4 - Added TitleViewHolder

---
+++ Chapter6/Exercise6.4/app/src/main/java/com/example/myrecyclerviewapp/TitleViewHolder.kt
@@ -0,0 +1,15 @@
+package com.example.myrecyclerviewapp
+
+import android.view.View
+import com.example.myrecyclerviewapp.model.ListItemUiModel
+import kotlinx.android.synthetic.main.item_title.item_title_title as titleView
+
+class TitleViewHolder(
+    override val containerView: View
+) : ListItemViewHolder(containerView) {
+    override fun bindData(listItem: ListItemUiModel) {
+        require(listItem is ListItemUiModel.Title) { "Expected ListItemUiModel.Title" }
+
+        titleView.text = listItem.title
+    }
+}
