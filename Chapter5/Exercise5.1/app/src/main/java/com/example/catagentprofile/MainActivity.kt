7fe7527c992dba5281e7ea4ff4053d9416f7b120
eranboudjnah <shadowcra@gmail.com>
2019-12-01 13:48:27 +0000

    Exercise 5.1 - Added Retrofit and the API service

--- Chapter5/Exercise5.1/app/src/main/java/com/example/catagentprofile/MainActivity.kt
+++ Chapter5/Exercise5.1/app/src/main/java/com/example/catagentprofile/MainActivity.kt
@@ -2,8 +2,19 @@ package com.example.catagentprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catagentprofile.api.TheCatApiService
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com/v1/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    }
+
+    private val theCatApiService by lazy { retrofit.create(TheCatApiService::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
