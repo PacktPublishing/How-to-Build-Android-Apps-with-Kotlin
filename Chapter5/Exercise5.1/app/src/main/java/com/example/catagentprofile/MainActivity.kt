a6013e36f830331e6246fb5bd1736e03efb638c9
eranboudjnah <shadowcra@gmail.com>
2019-12-01 13:55:23 +0000

    Exercise 5.1 - Added call to getImageResponse()

--- Chapter5/Exercise5.1/app/src/main/java/com/example/catagentprofile/MainActivity.kt
+++ Chapter5/Exercise5.1/app/src/main/java/com/example/catagentprofile/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getCatImageResponse()
     }
 
     private fun getCatImageResponse() {
