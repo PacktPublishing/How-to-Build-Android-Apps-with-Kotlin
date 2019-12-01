ded8cce00e3747a630513e8b6842177df9c492c3
eranboudjnah <shadowcra@gmail.com>
2019-12-01 13:47:25 +0000

    Exercise 5.1 - Added image search endpoint

--- Chapter5/Exercise5.1/app/src/main/java/com/example/catagentprofile/api/TheCatApiService.kt
+++ Chapter5/Exercise5.1/app/src/main/java/com/example/catagentprofile/api/TheCatApiService.kt
@@ -1,4 +1,13 @@
 package com.example.catagentprofile.api
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TheCatApiService {
+    @GET("images/search")
+    fun searchImages(
+        @Query("limit") limit: Int,
+        @Query("size") format: String
+    ) : Call<String>
 }
