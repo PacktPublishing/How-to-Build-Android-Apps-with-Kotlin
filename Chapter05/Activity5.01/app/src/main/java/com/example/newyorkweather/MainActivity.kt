4d5e0e4ac382f8de48c0e1e6467924d1177c2d5b
eranboudjnah <shadowcra@gmail.com>
2020-02-08 12:26:30 +0000

    Activity 5.1 - Added retrofit and service instances

--- Chapter05/Activity5.01/app/src/main/java/com/example/newyorkweather/MainActivity.kt
+++ Chapter05/Activity5.01/app/src/main/java/com/example/newyorkweather/MainActivity.kt
@@ -2,8 +2,20 @@ package com.example.newyorkweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newyorkweather.api.OpenWeatherMapService
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+    private val weatherApiService by lazy {
+        retrofit.create(OpenWeatherMapService::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
