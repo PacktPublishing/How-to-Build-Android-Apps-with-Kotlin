19222266fa98c028c736301fcaf315389a0a6db4
eranboudjnah <shadowcra@gmail.com>
2020-02-08 12:27:19 +0000

    Activity 5.1 - Added service call

--- Chapter05/Activity5.01/app/src/main/java/com/example/newyorkweather/MainActivity.kt
+++ Chapter05/Activity5.01/app/src/main/java/com/example/newyorkweather/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.newyorkweather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.newyorkweather.api.OpenWeatherMapService
+import com.example.newyorkweather.model.OpenWeatherMapResponseData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -20,5 +24,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherApiService
+            .getWeather("New York", "[YOUR TOKEN]")
+            .enqueue(object : Callback<OpenWeatherMapResponseData> {
+                override fun onFailure(call: Call<OpenWeatherMapResponseData>, t: Throwable) {
+                }
+
+                override fun onResponse(
+                    call: Call<OpenWeatherMapResponseData>,
+                    response: Response<OpenWeatherMapResponseData>
+                ) {
+                }
+            })
     }
 }
