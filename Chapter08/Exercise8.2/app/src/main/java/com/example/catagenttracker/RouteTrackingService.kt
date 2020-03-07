225f0fa083cc46f10ed51fa4b13057d478d2b47c
eranboudjnah <shadowcra@gmail.com>
2020-03-07 12:48:13 +0000

    Exercise 8.2 - Added function to create NotificationChannel

--- Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/RouteTrackingService.kt
+++ Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/RouteTrackingService.kt
@@ -1,14 +1,30 @@
 package com.example.catagenttracker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class RouteTrackingService : Service() {
     override fun onBind(intent: Intent): IBinder? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String {
+        val channelId = "routeTracking"
+        val channelName = "Route Tracking"
+        val channel =
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(channel)
+        return channelId
+    }
 
     private fun getPendingIntent() =
         PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), 0)
