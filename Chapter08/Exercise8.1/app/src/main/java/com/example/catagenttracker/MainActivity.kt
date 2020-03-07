dccfbca4ad6721012e560e0e5cf3c9e21e05c749
eranboudjnah <shadowcra@gmail.com>
2020-03-07 12:28:42 +0000

    Exercise 8.1 - Defined Worker requests

--- Chapter08/Exercise8.1/app/src/main/java/com/example/catagenttracker/MainActivity.kt
+++ Chapter08/Exercise8.1/app/src/main/java/com/example/catagenttracker/MainActivity.kt
@@ -2,13 +2,49 @@ package com.example.catagenttracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.Constraints
 import androidx.work.Data
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import com.example.catagenttracker.worker.CatFurGroomingWorker
+import com.example.catagenttracker.worker.CatLitterBoxSittingWorker
+import com.example.catagenttracker.worker.CatStretchingWorker
+import com.example.catagenttracker.worker.CatSuitUpWorker
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val networkConstraints =
+            Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+
+        val catAgentId = "CatAgent1"
+        val catStretchingRequest = OneTimeWorkRequest.Builder(CatStretchingWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(
+                getCatAgentIdInputData(CatStretchingWorker.INPUT_DATA_CAT_AGENT_ID, catAgentId)
+            ).build()
+        val catFurGroomingRequest = OneTimeWorkRequest.Builder(CatFurGroomingWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(
+                getCatAgentIdInputData(CatFurGroomingWorker.INPUT_DATA_CAT_AGENT_ID, catAgentId)
+            ).build()
+        val catLitterBoxSittingRequest =
+            OneTimeWorkRequest.Builder(CatLitterBoxSittingWorker::class.java)
+                .setConstraints(networkConstraints)
+                .setInputData(
+                    getCatAgentIdInputData(
+                        CatLitterBoxSittingWorker.INPUT_DATA_CAT_AGENT_ID,
+                        catAgentId
+                    )
+                ).build()
+        val catSuitUpRequest = OneTimeWorkRequest.Builder(CatSuitUpWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(
+                getCatAgentIdInputData(CatSuitUpWorker.INPUT_DATA_CAT_AGENT_ID, catAgentId)
+            ).build()
     }
 
     private fun getCatAgentIdInputData(catAgentIdKey: String, catAgentIdValue: String) =
