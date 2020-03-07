d1a0703a828760b6a156f8696cb699e030f10147
eranboudjnah <shadowcra@gmail.com>
2020-03-07 12:50:02 +0000

    Exercise 8.2 - Created function to start foreground service

--- Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/RouteTrackingService.kt
+++ Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/RouteTrackingService.kt
@@ -16,6 +16,20 @@ import androidx.lifecycle.MutableLiveData
 class RouteTrackingService : Service() {
     override fun onBind(intent: Intent): IBinder? = null
 
+    private fun startForegroundService(): NotificationCompat.Builder {
+        val pendingIntent = getPendingIntent()
+
+        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        } else {
+            ""
+        }
+
+        val notificationBuilder = getNotificationBuilder(pendingIntent, channelId)
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        return notificationBuilder
+    }
+
     private fun getNotificationBuilder(pendingIntent: PendingIntent, channelId: String) =
         NotificationCompat.Builder(this, channelId)
             .setContentTitle("Agent approaching destination")
