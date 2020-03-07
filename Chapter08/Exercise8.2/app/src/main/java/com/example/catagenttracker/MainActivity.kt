9d817d4cec33d2181f9f2941db659e74833d3b41
eranboudjnah <shadowcra@gmail.com>
2020-03-07 13:37:39 +0000

    Exercise 8.2 - Added function to launch the tracking Service

--- Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/MainActivity.kt
+++ Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.catagenttracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.work.Constraints
 import androidx.work.Data
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
+import com.example.catagenttracker.RouteTrackingService.Companion.EXTRA_SECRET_CAT_AGENT_ID
 import com.example.catagenttracker.worker.CatFurGroomingWorker
 import com.example.catagenttracker.worker.CatLitterBoxSittingWorker
 import com.example.catagenttracker.worker.CatStretchingWorker
@@ -84,6 +87,16 @@ class MainActivity : AppCompatActivity() {
             .then(catLitterBoxSittingRequest)
             .then(catSuitUpRequest)
             .enqueue()
+    }
+
+    private fun launchTrackingService() {
+        RouteTrackingService.trackingCompletion.observe(this, Observer { agentId ->
+            showResult("Agent $agentId arrived!")
+        })
+        val serviceIntent = Intent(this, RouteTrackingService::class.java).apply {
+            putExtra(EXTRA_SECRET_CAT_AGENT_ID, "007")
+        }
+        ContextCompat.startForegroundService(this, serviceIntent)
     }
 
     private fun showResult(message: String) {
