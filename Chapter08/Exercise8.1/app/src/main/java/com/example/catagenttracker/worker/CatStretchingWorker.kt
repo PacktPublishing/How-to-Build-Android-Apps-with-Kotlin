1d38803d8e83af42daff0f10915cc56cc34f2c66
eranboudjnah <shadowcra@gmail.com>
2020-03-07 12:20:07 +0000

    Exercise 8.1 - Implemented Worker behavior

--- Chapter08/Exercise8.1/app/src/main/java/com/example/catagenttracker/worker/CatStretchingWorker.kt
+++ Chapter08/Exercise8.1/app/src/main/java/com/example/catagenttracker/worker/CatStretchingWorker.kt
@@ -1,4 +1,25 @@
 package com.example.catagenttracker.worker
 
-class CatStretchingWorker {
+import android.content.Context
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class CatStretchingWorker(
+    context: Context,
+    workerParameters: WorkerParameters
+) : Worker(context, workerParameters) {
+    override fun doWork(): Result {
+        val catAgentId = inputData.getString(INPUT_DATA_CAT_AGENT_ID)
+        Thread.sleep(3000L)
+        val outputData = Data.Builder()
+            .putString(OUTPUT_DATA_CAT_AGENT_ID, catAgentId)
+            .build()
+        return Result.success(outputData)
+    }
+
+    companion object {
+        const val INPUT_DATA_CAT_AGENT_ID = "inId"
+        const val OUTPUT_DATA_CAT_AGENT_ID = "outId"
+    }
 }
