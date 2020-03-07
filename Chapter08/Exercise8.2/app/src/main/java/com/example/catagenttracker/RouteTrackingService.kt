d2f411c3dc7dfdfc6c77bcbe16877d2c4153b2de
eranboudjnah <shadowcra@gmail.com>
2020-03-07 12:45:32 +0000

    Exercise 8.2 - Added constants and LiveData instance

--- Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/RouteTrackingService.kt
+++ Chapter08/Exercise8.2/app/src/main/java/com/example/catagenttracker/RouteTrackingService.kt
@@ -3,7 +3,17 @@ package com.example.catagenttracker
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 class RouteTrackingService : Service() {
     override fun onBind(intent: Intent): IBinder? = null
+
+    companion object {
+        const val NOTIFICATION_ID = 0xCA7
+        const val EXTRA_SECRET_CAT_AGENT_ID = "scaId"
+
+        private val mutableTrackingCompletion = MutableLiveData<String>()
+        val trackingCompletion: LiveData<String> = mutableTrackingCompletion
+    }
 }
