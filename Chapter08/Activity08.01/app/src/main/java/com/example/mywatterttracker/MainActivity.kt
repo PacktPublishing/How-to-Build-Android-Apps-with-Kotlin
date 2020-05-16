febea3db16463caabad6271fe8a41f4edd3a8168
eranboudjnah <shadowcra@gmail.com>
2020-05-16 12:03:31 +0100

    Activity 8.1 - Added function to launch intent

--- Chapter08/Activity08.01/app/src/main/java/com/example/mywatterttracker/MainActivity.kt
+++ Chapter08/Activity08.01/app/src/main/java/com/example/mywatterttracker/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.mywatterttracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.mywatterttracker.WaterTrackingService.Companion.EXTRA_INTAKE_AMOUNT_MILLILITERS
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun launchTrackingService(intakeAmount: Float = 0f) {
+        val serviceIntent = Intent(this, WaterTrackingService::class.java).apply {
+            putExtra(EXTRA_INTAKE_AMOUNT_MILLILITERS, intakeAmount)
+        }
+        ContextCompat.startForegroundService(this, serviceIntent)
     }
 }
