84f7c48f041ce554134cf4c25ec2678f77a7584c
eranboudjnah <shadowcra@gmail.com>
2020-05-16 11:58:43 +0100

    Activity 8.1 - Added notification functions

--- Chapter08/Activity08.01/app/src/main/java/com/example/mywatterttracker/WaterTrackingService.kt
+++ Chapter08/Activity08.01/app/src/main/java/com/example/mywatterttracker/WaterTrackingService.kt
@@ -1,9 +1,15 @@
 package com.example.mywatterttracker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.IBinder
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 
 class WaterTrackingService : Service() {
@@ -12,6 +18,28 @@ class WaterTrackingService : Service() {
     private lateinit var serviceHandler: Handler
 
     override fun onBind(intent: Intent?): IBinder? = null
+
+    private fun getPendingIntent() =
+        PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), 0)
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String {
+        val channelId = "FluidBalanceTracking"
+        val channelName = "Fluid Balance Tracking"
+        val channel =
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(channel)
+        return channelId
+    }
+
+    private fun getNotificationBuilder(pendingIntent: PendingIntent, channelId: String) =
+        NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Tracking your fluid balance")
+            .setContentText("Tracking")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .setTicker("Fluid balance tracking started")
 
     companion object {
         const val EXTRA_INTAKE_AMOUNT_MILLILITERS = "intake"
