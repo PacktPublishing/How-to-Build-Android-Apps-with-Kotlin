08b9b275adcc1cf86f8b92473f5435ab777a81aa
eranboudjnah <shadowcra@gmail.com>
2020-05-16 11:57:16 +0100

    Activity 8.1 - Added required fields

--- Chapter08/Activity08.01/app/src/main/java/com/example/mywatterttracker/WaterTrackingService.kt
+++ Chapter08/Activity08.01/app/src/main/java/com/example/mywatterttracker/WaterTrackingService.kt
@@ -2,9 +2,15 @@ package com.example.mywatterttracker
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 
 class WaterTrackingService : Service() {
+    private var fluidBalanceMilliliters = 0f
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+    private lateinit var serviceHandler: Handler
+
     override fun onBind(intent: Intent?): IBinder? = null
 }
 
