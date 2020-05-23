12f07c08a0d74c4534ae1899d9eb8d194b4f9e33
alexforrester <aforresterwork@gmail.com>
2020-05-23 17:54:49 +0100

    Tidy ups of Tab Navigation

--- Chapter04/Exercise4.03/app/src/main/java/com/example/tabnavigation/MainActivity.kt
+++ Chapter04/Exercise4.03/app/src/main/java/com/example/tabnavigation/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.example.tabnavigation
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,42 +13,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-
         viewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
+        viewPager.adapter = MovieGenresPagerAdapter(this, supportFragmentManager)
 
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.tabMode = TabLayout.MODE_SCROLLABLE
+        tabs.tabMode = TabLayout.MODE_FIXED
         tabs.setupWithViewPager(viewPager)
-
-//        Add tab icons
-//        for (i in 0 until tabs.tabCount) {
-//            tabs.getTabAt(i)?.setIcon(R.drawable.movie_icon)
-//        }
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        super.onOptionsItemSelected(item)
-
-        if (item.itemId == R.id.nav_settings) {
-
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-        }
-
-        return true
     }
 }
