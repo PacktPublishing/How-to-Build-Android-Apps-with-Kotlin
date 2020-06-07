b5e237db9242fafca0fa238a3514977bb7c12741
Alex Dumbravan <alexandru.dumbravan@gmail.com>
2020-06-07 01:06:43 +0100

    Added Exercise12.03 and Exercise12.04

---
+++ Chapter12/Exercise12.03/app/src/main/java/com/android/testable/randomapplication/MainScope.kt
@@ -0,0 +1,8 @@
+package com.android.testable.randomapplication
+
+import javax.inject.Scope
+
+@Scope
+@MustBeDocumented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class MainScope
