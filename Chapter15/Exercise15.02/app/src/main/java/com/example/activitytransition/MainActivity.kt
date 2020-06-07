5c113e9e76e7b4a8a97c9e01fb14b93a3a3230e6
Jomar Tigcal <jomar@tigcal.com>
2020-06-06 21:34:58 -0700

    Changing Exercise 5 to 4 and updating the other projects with dependencies to match the Chapter content

--- Chapter15/Exercise15.02/app/src/main/java/com/example/activitytransition/MainActivity.kt
+++ Chapter15/Exercise15.02/app/src/main/java/com/example/activitytransition/MainActivity.kt
@@ -3,21 +3,17 @@ package com.example.activitytransition
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView = findViewById<ImageView>(R.id.image)
-
-        val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
             val intent = Intent(this@MainActivity, EndActivity::class.java)
-            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this, imageView, "transition_name").toBundle());
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this, image, "transition_name").toBundle());
         }
     }
 }
