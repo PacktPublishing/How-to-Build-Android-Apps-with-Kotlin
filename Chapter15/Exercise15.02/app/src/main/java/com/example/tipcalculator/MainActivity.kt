ba6fe8dd3cfb74561aecc3f5fd5b2b3fef917d0c
Jomar Tigcal <jomar@tigcal.com>
2020-06-08 19:11:58 -0700

    Code improvements

--- Chapter15/Exercise15.02/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ Chapter15/Exercise15.02/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.tipcalculator
 
 import android.app.ActivityOptions
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         compute_button.setOnClickListener {
-            val amount = if (amount_text.text.toString().isNotBlank()) amount_text.text.toString() else "0"
-            val percent = if (percent_text.text.toString().isNotBlank()) percent_text.text.toString() else "0"
+            val amount =
+                if (amount_text.text.toString().isNotBlank()) amount_text.text.toString() else "0"
+            val percent =
+                if (percent_text.text.toString().isNotBlank()) percent_text.text.toString() else "0"
             val intent = Intent(this, OutputActivity::class.java).apply {
                 putExtra("amount", amount)
                 putExtra("percent", percent)
             }
-            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this, image, "transition_name").toBundle());
+
+            startActivity(
+                intent,
+                ActivityOptions.makeSceneTransitionAnimation(this, image, "transition_name")
+                    .toBundle()
+            )
         }
     }
 
--- Chapter15/Exercise15.04/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ Chapter15/Exercise15.04/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -12,13 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         compute_button.setOnClickListener {
-            val amount = if (amount_text.text.toString().isNotBlank()) amount_text.text.toString() else "0"
-            val percent = if (percent_text.text.toString().isNotBlank()) percent_text.text.toString() else "0"
+            val amount =
+                if (amount_text.text.toString().isNotBlank()) amount_text.text.toString() else "0"
+            val percent =
+                if (percent_text.text.toString().isNotBlank()) percent_text.text.toString() else "0"
             val intent = Intent(this, OutputActivity::class.java).apply {
                 putExtra("amount", amount)
                 putExtra("percent", percent)
             }
-            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this, image, "transition_name").toBundle());
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this, image, "transition_name")
+                    .toBundle()
+            );
         }
     }
 
