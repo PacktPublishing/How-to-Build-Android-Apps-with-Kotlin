c3c857c86eeb20ed693190850c1bb6ef135e1ece
Eran Boudjnah <eranbou@gmail.com>
2020-07-11 15:29:42 +0100

    Changed example from cats to employees.

--- Chapter06/Exercise6.02/app/src/main/java/com/example/myrecyclerviewapp/model/EmployeeUiModel.kt
+++ Chapter06/Exercise6.02/app/src/main/java/com/example/myrecyclerviewapp/model/EmployeeUiModel.kt
@@ -1,9 +1,9 @@
 package com.example.myrecyclerviewapp.model
 
-data class CatUiModel(
-    val gender: Gender,
-    val breed: CatBreed,
+data class EmployeeUiModel(
     val name: String,
     val biography: String,
+    val role: EmployeeRole,
+    val gender: Gender,
     val imageUrl: String
 )
