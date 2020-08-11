ffed61d8c912bd5556122d10a03e7b7835a25abd
Jomar Tigcal <jomar@tigcal.com>
2020-08-10 20:03:19 -0700

    Adding Exercise 13.01

---
+++ Chapter13/Exercise13.01/app/src/main/java/com/example/popularmovies/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.popularmovies
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.popularmovies.api.MovieService
+import com.example.popularmovies.model.Movie
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    private val apiKey = "your_api_key_here"
+
+    private val movies = arrayListOf<Movie>()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+    }
+
+    private val movieService by lazy { retrofit.create(MovieService::class.java) }
+
+    private val movieAdapter by lazy {
+        MovieAdapter(movies, object : MovieAdapter.MovieClickListener {
+            override fun onMovieClick(movie: Movie) {
+                openMovieDetails(movie)
+            }
+        })
+    }
+
+    lateinit var disposable: Disposable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        movie_list.adapter = movieAdapter
+
+        getMovies()
+    }
+
+    private fun getMovies() {
+        disposable = movieService.getPopularMovies(apiKey)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .flatMap { Observable.fromIterable(it.results) }
+            .subscribe({ movie ->
+                movies.add(movie)
+                movieAdapter.notifyDataSetChanged()
+            }, { error ->
+                Log.d("Popular Movies", "error encountered: ${error.localizedMessage}")
+            })
+    }
+
+    private fun openMovieDetails(movie: Movie) {
+        val intent = Intent(this, DetailsActivity::class.java).apply {
+            putExtra(DetailsActivity.EXTRA_MOVIE, movie)
+        }
+        startActivity(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
+    }
+}
