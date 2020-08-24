87599f386a2433f197e90ca373ecb0b86912f8c6
Jomar Tigcal <jomar@tigcal.com>
2020-08-23 21:38:53 -0700

    Adding Exercise 14.01

---
+++ Chapter14/Exercise14.01/app/src/main/java/com/example/popularmovies/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.popularmovies
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.popularmovies.databinding.ActivityMainBinding
+import com.example.popularmovies.model.Movie
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val movies = arrayListOf<Movie>()
+
+    private val movieAdapter by lazy {
+        MovieAdapter(movies, object : MovieAdapter.MovieClickListener {
+            override fun onMovieClick(movie: Movie) {
+                openMovieDetails(movie)
+            }
+        })
+    }
+
+    private val movieViewModel by lazy {
+        ViewModelProvider(this).get(MovieViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewModel = movieViewModel
+        binding.lifecycleOwner = this
+
+        movie_list.adapter = movieAdapter
+    }
+
+    private fun openMovieDetails(movie: Movie) {
+        val intent = Intent(this, DetailsActivity::class.java).apply {
+            putExtra(DetailsActivity.EXTRA_MOVIE, movie)
+        }
+        startActivity(intent)
+    }
+}
