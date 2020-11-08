b137d6ff07fb2bc92d30938f184d7a7a8e2c9cf7
Alex Dumbravan <alexandru.dumbravan@gmail.com>
2020-11-08 19:45:07 +0000

    Chapter 11: Added handling for the response body

--- Chapter11/Activity11.01/app/src/main/java/com/android/testable/remote_media_provider/repository/DownloadRepositoryImpl.kt
+++ Chapter11/Activity11.01/app/src/main/java/com/android/testable/remote_media_provider/repository/DownloadRepositoryImpl.kt
@@ -69,12 +69,14 @@ class DownloadRepositoryImpl(
                     if (response.isSuccessful) {
                         executor.execute {
                             try {
-                                val name = url.substring(url.lastIndexOf("/") + 1)
-                                providerFileHandler.writeStream(
-                                    name,
-                                    response.body()!!.byteStream()
-                                )
-                                result.postValue(Result.Success(Unit))
+                                response.body()?.let {
+                                    val name = url.substring(url.lastIndexOf("/") + 1)
+                                    providerFileHandler.writeStream(
+                                        name,
+                                        response.body()!!.byteStream()
+                                    )
+                                    result.postValue(Result.Success(Unit))
+                                }
                             } catch (e: Exception) {
                                 e.printStackTrace()
                                 result.postValue(Result.Error())
