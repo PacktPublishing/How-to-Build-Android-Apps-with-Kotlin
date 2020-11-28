fb6a1fcbc5129bce14f956ffe8270524ad9129bd
Eran Boudjnah <eranbou@gmail.com>
2020-11-28 17:55:34 +0000

    [Chapter06/Activity6.01] Updated dependencies and code to latest conventions.

--- Chapter06/Activity6.01/app/src/main/java/com/example/recipebook/ViewHolder.kt
+++ Chapter06/Activity6.01/app/src/main/java/com/example/recipebook/ViewHolder.kt
@@ -1,32 +1,34 @@
 package com.example.recipebook
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipebook.model.ListItem
 import com.example.recipebook.model.RecipeUiModel
 import com.example.recipebook.model.TitleUiModel
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.item_recipe.view.*
-import kotlinx.android.synthetic.main.item_title.view.*
 
 abstract class BaseViewHolder(
-    override val containerView: View
-) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    containerView: View
+) : RecyclerView.ViewHolder(containerView) {
     abstract fun bindData(listItem: ListItem)
 }
 
-class TitleViewHolder(override val containerView: View) : BaseViewHolder(containerView) {
-    private val titleView by lazy { containerView.title_label }
+class TitleViewHolder(containerView: View) : BaseViewHolder(containerView) {
+    private val titleView: TextView
+            by lazy { containerView.findViewById(R.id.title_label) }
+
     override fun bindData(listItem: ListItem) {
         titleView.text = (listItem as TitleUiModel).title
     }
 }
 
 class RecipeViewHolder(
-    override val containerView: View,
+    containerView: View,
     private val onClickListener: OnClickListener
 ) : BaseViewHolder(containerView) {
-    private val titleView by lazy { containerView.recipe_title }
+    private val titleView: TextView
+            by lazy { containerView.findViewById(R.id.recipe_title) }
+
     override fun bindData(listItem: ListItem) {
         titleView.text = (listItem as RecipeUiModel).title
         titleView.setOnClickListener {
