3601ac7b34067f6f2074bcf0e7edbd505815c7d4
Eran Boudjnah <eranbou@gmail.com>
2020-11-29 13:25:13 +0000

    [Chapter05/Exercise6.06] Updated dependencies and code to latest conventions.

--- Chapter06/Exercise6.06/app/src/main/java/com/example/myrecyclerviewapp/viewholder/TitleViewHolder.kt
+++ Chapter06/Exercise6.06/app/src/main/java/com/example/myrecyclerviewapp/viewholder/TitleViewHolder.kt
@@ -1,12 +1,16 @@
 package com.example.myrecyclerviewapp.viewholder
 
 import android.view.View
+import android.widget.TextView
+import com.example.myrecyclerviewapp.R
 import com.example.myrecyclerviewapp.model.ListItemUiModel
-import kotlinx.android.synthetic.main.item_title.item_title_title as titleView
 
 class TitleViewHolder(
-    override val containerView: View
+    containerView: View
 ) : ListItemViewHolder(containerView) {
+    private val titleView: TextView
+            by lazy { containerView.findViewById(R.id.item_title_title) }
+
     override fun bindData(listItem: ListItemUiModel) {
         require(listItem is ListItemUiModel.Title) { "Expected ListItemUiModel.Title" }
 
