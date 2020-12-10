6c812818ac13217a6e9394b8b934935c5f74e055
alexforrester <alexander_forrester@discovery.com>
2020-12-10 18:58:28 +0000

    Updating dependencies and minor code changes

--- Chapter03/Exercise3.01/app/src/main/java/com/example/fragmentlifecycle/MainFragment.kt
+++ Chapter03/Exercise3.01/app/src/main/java/com/example/fragmentlifecycle/MainFragment.kt
@@ -1,28 +1,43 @@
 package com.example.fragmentlifecycle
 
 import android.content.Context
-import android.os.Bundle
 import android.util.Log
+import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
  */
 class MainFragment : Fragment() {
 
     private val TAG = "MainFragment"
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        Log.d(TAG, "onAttach")
-    }
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach")
     }
 
     override fun onCreateView(
@@ -78,6 +93,26 @@ class MainFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         Log.d(TAG, "onDetach")
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 
 }
--- Chapter03/Exercise3.03/app/src/main/java/com/example/staticfragments/DetailFragment.kt
+++ Chapter03/Exercise3.03/app/src/main/java/com/example/staticfragments/DetailFragment.kt
@@ -1,14 +1,13 @@
 package com.example.staticfragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_detail.*
-
-
 
 class DetailFragment : Fragment() {
 
@@ -23,74 +22,74 @@ class DetailFragment : Fragment() {
 
     fun setStarSignData(starSignId: Int) {
 
-            //Some text below should in production be string resources, done as hardcoded text here for simplicity
-            when (starSignId) {
-                R.id.aquarius -> {
-                    star_sign.text = getString(R.string.aquarius)
-                    symbol.text = getString(R.string.symbol,"Water Carrier")
-                    date_range.text = getString(R.string.date_range,"January 20 - February 18")
-                }
-                R.id.pisces -> {
-                    star_sign.text = getString(R.string.pisces)
-                    symbol.text = getString(R.string.symbol, "Fish")
-                    date_range.text = getString(R.string.date_range,"February 19 - March 20")
-                }
-                R.id.aries -> {
-                    star_sign.text = getString(R.string.aries)
-                    symbol.text = getString(R.string.symbol,"Ram")
-                    date_range.text = getString(R.string.date_range,"March 21 - April 19")
-                }
-                R.id.taurus -> {
-                    star_sign.text = getString(R.string.taurus)
-                    symbol.text = getString(R.string.symbol,"Bull")
-                    date_range.text = getString(R.string.date_range,"April 20 - May 20")
-                }
-                R.id.gemini -> {
-                    star_sign.text = getString(R.string.gemini)
-                    symbol.text = getString(R.string.symbol,"Twins")
-                    date_range.text = getString(R.string.date_range,"May 21 - June 20")
-                }
-                R.id.cancer -> {
-                    star_sign.text = getString(R.string.cancer)
-                    symbol.text = getString(R.string.symbol,"Crab")
-                    date_range.text = getString(R.string.date_range,"June 21 - July 22")
-                }
-                R.id.leo  -> {
-                    star_sign.text = getString(R.string.capricorn)
-                    symbol.text = getString(R.string.symbol,"Lion")
-                    date_range.text = getString(R.string.date_range,"July 23 - August 22")
-                }
-                R.id.virgo  -> {
-                    star_sign.text = getString(R.string.virgo)
-                    symbol.text =getString(R.string.symbol, "Virgin")
-                    date_range.text = getString(R.string.date_range,"August 23 - September 22")
-                }
-                R.id.libra  -> {
-                    star_sign.text = getString(R.string.libra)
-                    symbol.text = getString(R.string.symbol,"Scales")
-                    date_range.text = getString(R.string.date_range,"September 23 - October 22")
-                }
-                R.id.scorpio  -> {
-                    star_sign.text = getString(R.string.scorpio)
-                    symbol.text = getString(R.string.symbol,"Scorpion")
-                    date_range.text = getString(R.string.date_range,"October 23 - November 21")
-                }
-                R.id.sagittarius -> {
-                    star_sign.text = getString(R.string.sagittarius)
-                    symbol.text = getString(R.string.symbol,"Archer")
-                    date_range.text = getString(R.string.date_range,"November 22 - December 21")
-                }
-                R.id.capricorn -> {
-                    star_sign.text = getString(R.string.capricorn)
-                    symbol.text = getString(R.string.symbol,"Mountain Goat")
-                    date_range.text = getString(R.string.date_range,"December 22 - January 19")
-                }
-                else -> {
-                    Toast.makeText(context, getString(R.string.unknown_star_sign), Toast.LENGTH_LONG).show();
-                }
+        //Some text below should in production be string resources, done as hardcoded text here for simplicity
+        when (starSignId) {
+            R.id.aquarius -> {
+                star_sign.text = getString(R.string.aquarius)
+                symbol.text = getString(R.string.symbol, "Water Carrier")
+                date_range.text = getString(R.string.date_range, "January 20 - February 18")
             }
+            R.id.pisces -> {
+                star_sign.text = getString(R.string.pisces)
+                symbol.text = getString(R.string.symbol, "Fish")
+                date_range.text = getString(R.string.date_range, "February 19 - March 20")
+            }
+            R.id.aries -> {
+                star_sign.text = getString(R.string.aries)
+                symbol.text = getString(R.string.symbol, "Ram")
+                date_range.text = getString(R.string.date_range, "March 21 - April 19")
+            }
+            R.id.taurus -> {
+                star_sign.text = getString(R.string.taurus)
+                symbol.text = getString(R.string.symbol, "Bull")
+                date_range.text = getString(R.string.date_range, "April 20 - May 20")
+            }
+            R.id.gemini -> {
+                star_sign.text = getString(R.string.gemini)
+                symbol.text = getString(R.string.symbol, "Twins")
+                date_range.text = getString(R.string.date_range, "May 21 - June 20")
+            }
+            R.id.cancer -> {
+                star_sign.text = getString(R.string.cancer)
+                symbol.text = getString(R.string.symbol, "Crab")
+                date_range.text = getString(R.string.date_range, "June 21 - July 22")
+            }
+            R.id.leo -> {
+                star_sign.text = getString(R.string.capricorn)
+                symbol.text = getString(R.string.symbol, "Lion")
+                date_range.text = getString(R.string.date_range, "July 23 - August 22")
+            }
+            R.id.virgo -> {
+                star_sign.text = getString(R.string.virgo)
+                symbol.text = getString(R.string.symbol, "Virgin")
+                date_range.text = getString(R.string.date_range, "August 23 - September 22")
+            }
+            R.id.libra -> {
+                star_sign.text = getString(R.string.libra)
+                symbol.text = getString(R.string.symbol, "Scales")
+                date_range.text = getString(R.string.date_range, "September 23 - October 22")
+            }
+            R.id.scorpio -> {
+                star_sign.text = getString(R.string.scorpio)
+                symbol.text = getString(R.string.symbol, "Scorpion")
+                date_range.text = getString(R.string.date_range, "October 23 - November 21")
+            }
+            R.id.sagittarius -> {
+                star_sign.text = getString(R.string.sagittarius)
+                symbol.text = getString(R.string.symbol, "Archer")
+                date_range.text = getString(R.string.date_range, "November 22 - December 21")
+            }
+            R.id.capricorn -> {
+                star_sign.text = getString(R.string.capricorn)
+                symbol.text = getString(R.string.symbol, "Mountain Goat")
+                date_range.text = getString(R.string.date_range, "December 22 - January 19")
+            }
+            else -> {
+                Toast.makeText(context, getString(R.string.unknown_star_sign), Toast.LENGTH_LONG)
+                    .show();
+            }
+        }
 
     }
-
 
 }
