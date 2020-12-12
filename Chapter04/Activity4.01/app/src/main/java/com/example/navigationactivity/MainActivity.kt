fb804aa4021e491133b71d21ac7aaddafc2b6e9e
Alex Forrester <alexander_forrester@discovery.com>
2020-12-12 20:56:58 +0000

    chapter 04 updates - Updating dependencies and minor code changes (#18)
    * Updating dependencies and minor code changes
    
    * Updates dependencies and minor code updates
    
    * Updates to use synthetics view access
    
    * Updates dependencies for navigation components
    
    * Removes unnecessary .idea files
    
    * Reverts .gitignore files with unnecessary entries
    
    * Removes uneccessary files from .gitignore
    
    * Updates .gitignore with correct entries
    
    * Add futher entry to .gitignore
    
    * Adds further entry to .gitignore

--- Chapter04/Activity4.01/app/src/main/java/com/example/navigationactivity/MainActivity.kt
+++ Chapter04/Activity4.01/app/src/main/java/com/example/navigationactivity/MainActivity.kt
@@ -2,13 +2,12 @@ package com.example.navigationactivity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,15 +16,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
 
         //Creating top level destinations and adding them to bottom navigation
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_account, R.id.nav_profile, R.id.nav_mysports))
+            R.id.nav_home, R.id.nav_account, R.id.nav_profile, R.id.nav_mysports))
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        nav_view?.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- Chapter04/Exercise4.02/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ Chapter04/Exercise4.02/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.bottomnavigation
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,23 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
 
         //Creating top level destinations and adding them to bottom navigation
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_tickets, R.id.nav_offers, R.id.nav_rewards))
+            R.id.nav_home, R.id.nav_tickets, R.id.nav_offers, R.id.nav_rewards))
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        nav_view?.setupWithNavController(navController)
     }
-
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main, menu)
