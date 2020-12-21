68dc3dc330e5496465245e77295f8eca287d95f1
alexforrester <alexander_forrester@discovery.com>
2020-12-21 22:49:57 +0000

    Tidies up tab layouts

--- Chapter04/Exercise4.03/app/src/main/java/com/example/tabnavigation/MainActivity.kt
+++ Chapter04/Exercise4.03/app/src/main/java/com/example/tabnavigation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tabnavigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.INDICATOR_GRAVITY_TOP
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +18,6 @@ class MainActivity : AppCompatActivity() {
 
         tabs?.tabMode = TabLayout.MODE_FIXED
         tabs?.setupWithViewPager(view_pager)
+        tabs.tabRippleColor = null
     }
 }
