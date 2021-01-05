6a01e0d28bd028c28e91260880bb842de69a311d
alexforrester <alexander_forrester@discovery.com>
2021-01-05 21:02:19 +0000

    Remove synthetic view access from Chapter 1

--- Chapter01/Exercise1.05/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Chapter01/Exercise1.05/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,38 +3,39 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        enter_button.setOnClickListener {
-            //Set the text to an empty string when the button is pressed initially
-            greeting_display.text = ""
+        findViewById<Button>(R.id.enter_button)?.setOnClickListener {
+            //Get the greeting display text
+            val greetingDisplay = findViewById<TextView>(R.id.greeting_display)
 
             //Get the first name TextInputEditText value
-            val firstName = first_name.text.toString().trim()
+            val firstName = findViewById<TextInputEditText>(R.id.first_name)?.text.toString().trim()
 
             //Get the last name TextInputEditText value
-            val lastName = last_name.text.toString().trim()
+            val lastName = findViewById<TextInputEditText>(R.id.last_name)?.text.toString().trim()
 
             //Check names are not empty here:
             if (firstName.isNotEmpty() && lastName.isNotEmpty()) {
 
                 val nameToDisplay = firstName.plus(" ").plus(lastName)
                 //Use Kotlin's string templates feature to display the name
-                greeting_display.text =
+                greetingDisplay?.text =
                     " ${getString(R.string.welcome_to_the_app)} ${nameToDisplay}!"
             } else {
-
-            Toast.makeText(this, getString(R.string.please_enter_a_name), Toast.LENGTH_LONG).
-                apply{
-                    setGravity(Gravity.CENTER, 0, 0)
-                    show()
-                }
+                Toast.makeText(this, getString(R.string.please_enter_a_name), Toast.LENGTH_LONG)
+                    .apply {
+                        setGravity(Gravity.CENTER, 0, 0)
+                        show()
+                    }
             }
         }
     }
